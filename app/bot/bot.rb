77971ae00a0b7e29868bb0320c77bcdf0bd494c6
EC2 Default User <ec2-user@ip-172-31-31-33.us-east-2.compute.internal>
2018-06-24 20:42:00 +0000

    Test Get Started

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,29 +3,33 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
+Bot.on :get_started do |get_started|
+  { 
+  "get_started":{
+    "payload":"<GET_STARTED_PAYLOAD>"
+  }
+}
+end
 
-Bot.on :message do |message|
-  message.id          # => 'mid.1457764197618:41d102a3e1ae206a38'
-  message.sender      # => { 'id' => '1008372609250235' }
-  message.seq         # => 73
-  message.sent_at     # => 2016-04-22 21:30:36 +0200
-  message.text        # => 'Hello, bot!'
-  message.attachments # => [ { 'type' => 'image', 'payload' => { 'url' => 'https://www.example.com/1.jpg' } } ]
-  
-  message.reply(
-  attachment: {
-    type: 'template',
-    payload: {
-      template_type: 'button',
-      text: 'Human, do you like me?',
-      buttons: [
-        { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
-        { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
-                ]
-            }
-                }
-)
-    Bot.on :postback do |postback|
+def message_answer
+  Bot.on :message do |message|
+    message.reply(
+    attachment: {
+      type: 'template',
+      payload: {
+        template_type: 'button',
+        text: 'Human, do you like me?',
+        buttons: [
+          { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
+          { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
+                  ]
+              }
+                  }
+   )
+    
+  end
+
+Bot.on :postback do |postback|
       postback.sender    # => { 'id' => '1008372609250235' }
       postback.recipient # => { 'id' => '2015573629214912' }
       postback.sent_at   # => 2016-04-22 21:30:36 +0200
@@ -35,5 +39,3 @@ Bot.on :message do |message|
         puts "Human #{postback.recipient} marked for extermination"
       end
     end
-end
-
