29ba0bdf7eaf1b35c9e1dc0951b6df681b20edfa
EC2 Default User <ec2-user@ip-172-31-31-33.us-east-2.compute.internal>
2018-06-24 13:47:58 +0000

    Test Button

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -5,5 +5,17 @@ Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
 
 Bot.on :message do |message|
-  message.reply(text: 'Hello, human!')
+  message.reply(
+  attachment: {
+    type: 'template',
+    payload: {
+      template_type: 'button',
+      text: 'Human, do you like me?',
+      buttons: [
+        { type: 'postback', title: 'Yes', payload: 'HARMLESS' },
+        { type: 'postback', title: 'No', payload: 'EXTERMINATE' }
+      ]
+    }
+  }
+)
 end
