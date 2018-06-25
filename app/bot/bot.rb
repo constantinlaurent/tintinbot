e34f33276ac2c9867b60ae54e46543488c4e4d1f
EC2 Default User <ec2-user@ip-172-31-31-33.us-east-2.compute.internal>
2018-06-25 11:53:58 +0000

    Get Started

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -3,6 +3,12 @@ include Facebook::Messenger
 
 Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
 
+Facebook::Messenger::Profile.set({
+  get_started: {
+    payload: 'GET_STARTED_PAYLOAD'
+  }
+}, access_token: ENV['ACCESS_TOKEN'])
+
 Bot.on :message do |message|
   message.reply(
   attachment: {
